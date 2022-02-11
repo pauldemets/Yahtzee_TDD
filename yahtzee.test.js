d9f264e3ea4a4a2a31e03feab6ed1b9cf84293f2
Paul Demets <paul.demets@hotmail.com>
2022-02-11 17:11:12 +0100

    handle sixes

--- yahtzee.test.js
+++ yahtzee.test.js
@@ -30,3 +30,10 @@ test('test fives', () => {
     const dices2 = [6, 5, 5, 5, 5];
     expect(yahtzee.handleBasic(dices2, 5)).toBe(20);
 });
+
+test('test sixes', () => {
+    const dices = [5, 5, 6, 6, 6];
+    expect(yahtzee.handleBasic(dices, 6)).toBe(18);
+    const dices2 = [2, 6, 2, 6, 5];
+    expect(yahtzee.handleBasic(dices2, 6)).toBe(12);
+});
