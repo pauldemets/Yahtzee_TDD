000a9d05a0ed3464b6dd8127f542d890ef94df3c
Paul Demets <paul.demets@hotmail.com>
2022-02-11 17:08:06 +0100

    refacto

--- yahtzee.js
+++ yahtzee.js
@@ -6,46 +6,25 @@ const DICES = [
     1
 ];
 
-const handleAces = (dices) => {
+
+
+const handleBasic = (dices, number) => {
     var cpt = 0;
     dices.forEach(dice => {
-        if (dice === 1) {
+        if (dice === number) {
             cpt++;
         }
     });
-
-    return cpt;
-}
-
-const handleTwos = (dices) => {
-    var cpt = 0;
-    dices.forEach(dice => {
-        if (dice === 2) {
-            cpt++;
-        }
-    });
-
-    return cpt * 2;
-}
-
-const handleThrees = (dices) => {
-    var cpt = 0;
-    dices.forEach(dice => {
-        if (dice === 3) {
-            cpt++;
-        }
-    });
-
-    return cpt * 3;
+    return cpt * number;
 }
 
 
 const main = () => {
-    handleAces(DICES);
+
 }
 
 
 main();
 
 
-module.exports = { handleAces, handleTwos, handleThrees };
+module.exports = { handleBasic };
--- yahtzee.test.js
+++ yahtzee.test.js
@@ -2,17 +2,17 @@ const yahtzee = require('./yahtzee');
 
 test('test aces', () => {
     const dices = [3, 4, 1, 2, 1];
-    expect(yahtzee.handleAces(dices)).toBe(2);
+    expect(yahtzee.handleBasic(dices, 1)).toBe(2);
 });
 
 test('test twos', () => {
     const dices = [2, 2, 1, 2, 1];
-    expect(yahtzee.handleTwos(dices)).toBe(6);
+    expect(yahtzee.handleBasic(dices, 2)).toBe(6);
 });
 
 test('test threes', () => {
     const dices = [2, 2, 1, 2, 1];
-    expect(yahtzee.handleThrees(dices)).toBe(0);
+    expect(yahtzee.handleBasic(dices, 3)).toBe(0);
     const dices2 = [6, 3, 3, 3, 1];
-    expect(yahtzee.handleThrees(dices2)).toBe(9);
+    expect(yahtzee.handleBasic(dices2, 3)).toBe(9);
 });
